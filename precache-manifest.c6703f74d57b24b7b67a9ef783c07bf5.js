0e7fe3cc3ee9e8cc65f240f4c6a4b8c425a29d3a
abdeloifi ahmed <abdeloifiahmed@hotmail.com>
2019-03-06 14:24:39 +0100

    Updates

---
+++ precache-manifest.c6703f74d57b24b7b67a9ef783c07bf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52d2e89d6d1aae702826",
+    "url": "/firstReactApp/static/js/runtime~main.52d2e89d.js"
+  },
+  {
+    "revision": "8636021e4510d8947d2b",
+    "url": "/firstReactApp/static/js/main.8636021e.chunk.js"
+  },
+  {
+    "revision": "642abf8ba4cabcc63c04",
+    "url": "/firstReactApp/static/js/2.642abf8b.chunk.js"
+  },
+  {
+    "revision": "8636021e4510d8947d2b",
+    "url": "/firstReactApp/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "642abf8ba4cabcc63c04",
+    "url": "/firstReactApp/static/css/2.da330ddf.chunk.css"
+  },
+  {
+    "revision": "845592968b21f7375672832cdedf6f46",
+    "url": "/firstReactApp/index.html"
+  }
+];
